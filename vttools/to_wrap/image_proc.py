5545161d9c3353d98b1731e3e2fe46a5c64ae0ae
Gabriel Iltis <giltis@bnl.gov>
2015-04-13 02:22:02 -0400

    DEV: Changed data inputs to x1 and x2. Completed docstrings for funcs.
    As per Eric's suggestion, inputs specific to data have been renamed in
    order to conform to typical numpy docstrings, e.g. the arg src_data1
    and src_data2 have been changed to x1 and x2.
    
    The funcs requiring a string operation to be defined have also been
    standardized so that the input order starts with the operation
    or expression followed by the input data.
    
    Finally, the docstring for arithmetic_custom was incomplete, and has
    been flushed out.

--- vttools/to_wrap/image_proc.py
+++ vttools/to_wrap/image_proc.py
@@ -13,9 +13,9 @@ from numpy import (logical_xor, logical_and, logical_or, logical_not, add,
                    subtract, multiply, divide)
 
 
-def arithmetic_basic(input_1,
-                     input_2,
-                     operation):
+def arithmetic_basic(operation,
+                     x1,
+                     x2):
     """
     This function enables basic arithmetic for image processing and data
     analysis. The function is capable of applying the basic arithmetic
@@ -24,26 +24,41 @@ def arithmetic_basic(input_1,
 
     Parameters
     ----------
-    input_1 : {ndarray, int, float}
-        Specifies the first input data set, or constant, to be offset or
-        manipulated
-
-    input_2 : {ndarray, int, float}
-        Specifies the second data set, or constant, to be offset or manipulated
-
     operation : string
-        addition: the addition of EITHER two images or volume data sets,
+        addition:
+            The addition of EITHER two images or volume data sets,
             OR an image/data set and a value. This function is typically
-            used for offset purposes, or basic recombination of several isolated
-            materials or phases into a single segmented volume.
-        subtraction: enables the subtraction of EITHER one image or volume data
+            used for offset purposes, or basic recombination of several
+            isolated materials or phases into a single segmented volume.
+        subtraction:
+            Enables the subtraction of EITHER one image or volume data
             set from another, OR reduction of all values in an image/data set
             by a set value. This function is typically used for offset
             purposes, or basic isolation of objects or materials/phases in a
             data set.
         multiplication:
+            Enables the multiplication of input 1 (x1) by input 2 (x2). The
+            inputs can be of any valid numpy data type (e.g. an image or
+            volume data a fixed, constant, value). This function is typically
+            used for offset purposes (rescaling), or for assigning values in
+            the generation of a labelfield identifying objects or
+            materials/phases in a data set.
 
         division:
+            Enables the division of input 1 (x1, numerator) by input 2 (x2,
+            denominator). The inputs can be of any valid numpy data type
+            (e.g. an image or volume data a fixed, constant, value). Basic
+            tests are included in the division function which test for,
+            and ensure that division by zero does not occur. This function is
+            typically used for offset purposes (rescaling, normalization).
+
+    x1 : {ndarray, int, float}
+        Specifies the first input data set, or constant, to be offset or
+        manipulated
+
+    x2 : {ndarray, int, float}
+        Specifies the second data set, or constant, to be offset or manipulated
+
 
 
     Returns
@@ -61,19 +76,19 @@ def arithmetic_basic(input_1,
                       'division' : divide
     }
     if operation == 'division':
-        if type(input_2) is np.ndarray:
-            if 0 in input_2:
+        if type(x2) is np.ndarray:
+            if 0 in x2:
                 raise ValueError("This division operation will result in "
                                  "division by zero values. Please reevaluate "
-                                 "denominator (input_2).")
+                                 "denominator (x2).")
         else:
-            if float(input_2) == 0:
+            if float(x2) == 0:
                 raise ValueError("This division operation will result in "
                                  "division by a zero value. Please "
                                  "reevaluate the denominator constant"
-                                 " (input_2).")
+                                 " (x2).")
 
-    output = operation_dict[operation](input_1, input_2)
+    output = operation_dict[operation](x1, x2)
     return output
 
 
@@ -174,8 +189,8 @@ def arithmetic_custom(expression,
 
 
 def logic_basic(operation,
-                src_data1,
-                src_data2=None):
+                x1,
+                x2=None):
     """
     This function enables the computation of the basic logical operations
     oft used in image processing of two image or volume  data sets. This
@@ -193,10 +208,10 @@ def logic_basic(operation,
             'nand' -- 2 inputs
             'subtract' -- 2 inputs
 
-    src_data1 : {ndarray, int, float, list, tuple}
+    x1 : {ndarray, int, float, list, tuple}
         Specifies the first reference
 
-    src_data2 : {ndarray, int, float, list, tuple}
+    x2 : {ndarray, int, float, list, tuple}
         Specifies the second reference
 
     Returns
@@ -217,8 +232,8 @@ def logic_basic(operation,
                   'nor' : logical_nor,
                   'subtract' : logical_sub
                   }
-    output = logic_dict[operation](src_data1,
-                                   src_data2)
+    output = logic_dict[operation](x1,
+                                   x2)
     return output
 
 __all__ = (add, subtract, multiply, divide, logical_and, logical_or,
